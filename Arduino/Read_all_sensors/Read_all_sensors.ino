59bf9a304a0d479697aaaa2b37c31e336147a036
BeritGreinke <68007113+beritgreinke@users.noreply.github.com>
2021-11-23 15:10:18 +0100

    Update Read_all_sensors.ino

--- Arduino/Read_all_sensors/Read_all_sensors.ino
+++ Arduino/Read_all_sensors/Read_all_sensors.ino
@@ -9,7 +9,7 @@
 
   http://www.arduino.cc/en/Tutorial/AnalogReadSerial
 
-  Edited by Berit Greinke on 28 March 2021
+  Edited by Berit Greinke on 5 Nov 2021 to read 6 analog and 4 digital sensors
 */
 
 // the setup routine runs once when you press reset:
@@ -27,13 +27,18 @@ void loop() {
   int sensorValue_A3 = analogRead(3);
   int sensorValue_A4 = analogRead(4);
   int sensorValue_A5 = analogRead(5);
+  
+  int sensorValue_D2= digitalRead(2);
+  int sensorValue_D3= digitalRead(3);
+  int sensorValue_D4= digitalRead(4);
+  int sensorValue_D5= digitalRead(5);
+  int sensorValue_D6= digitalRead(6);
+  int sensorValue_D7= digitalRead(7);
+  int sensorValue_D8= digitalRead(8);
+  int sensorValue_D9= digitalRead(9);
 
  // print out the value you read:
 
-  Serial.print(0);
-  Serial.print(" ");
-  Serial.print(1023);
-  Serial.print(" ");
   Serial.print(sensorValue_A0);
   Serial.print(" ");
   Serial.print(sensorValue_A1);
@@ -44,7 +49,23 @@ void loop() {
   Serial.print(" ");
   Serial.print(sensorValue_A4);
   Serial.print(" ");
-  Serial.println(sensorValue_A5);
+  Serial.print(sensorValue_A5);
+  Serial.print(" ");
+  Serial.print(sensorValue_D2);
+  Serial.print(" ");
+  Serial.print(sensorValue_D3);
+  Serial.print(" ");
+  Serial.print(sensorValue_D4);
+  Serial.print(" ");
+  Serial.print(sensorValue_D5);
+  Serial.print(" ");
+  Serial.print(sensorValue_D6);
+  Serial.print(" ");
+  Serial.print(sensorValue_D7);
+  Serial.print(" ");
+  Serial.print(sensorValue_D8);
+  Serial.print(" ");
+  Serial.println(sensorValue_D9);
 
   delay(10);        // delay in between reads for stability
 }
